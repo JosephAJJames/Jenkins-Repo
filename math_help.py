3c4abcea31242c34a056f0ad50fbc5e3383dc11d
jamesj9@lancaster.ac.uk <jamesj9@lancaster.ac.uk>
2024-09-10 18:31:02 +0100

    Updated functions

---
+++ math_help.py
@@ -0,0 +1,26 @@
+def add(num1: int, num2: int):
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        return "ensure parameters are integers"
+    
+    return num1 + num2
+
+def subtract(num1: int, num2: int):
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        return "ensure parameters are integers"
+
+    return num1 - num2
+
+def divide(num1: int, num2: int):
+    if not isinstance(num1, int) or not isinstance(num2, int):
+        return "ensure parameters are integers"
+
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        return "can't divide by 0"
+
+def sqrt(num1: int):
+    if not isinstance(num1, int):
+        return "ensure parameters are integers"
+    
+    return num1 ** 0.5
